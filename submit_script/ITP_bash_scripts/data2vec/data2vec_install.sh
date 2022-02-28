1848cfe4221f7bf1f3d17756541b19dd54680723
ddlBoJack <maziyang99@163.com>
2022-02-28 22:40:44 +0800

    update right scripts

--- submit_script/ITP_bash_scripts/data2vec/data2vec_install.sh
+++ submit_script/ITP_bash_scripts/data2vec/data2vec_install.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 set -x
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
-~/miniconda/bin/conda init $(echo $SHELL | awk -F '/' '{print $NF}')
-echo 'Successfully installed miniconda...'
-echo -n 'Conda version: '
-~/miniconda/bin/conda --version
-echo -e '\n'
 
-source ~/miniconda/etc/profile.d/conda.sh
-conda env list
-echo 'Create fairseq env...'
-conda create -n fairseq python=3.8
-conda init $(echo $SHELL | awk -F '/' '{print $NF}')
-conda env list
-echo 'Activate fairseq env...'
-conda activate fairseq
-conda info --env
+# This block install conda.
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+# ~/miniconda/bin/conda init $(echo $SHELL | awk -F '/' '{print $NF}')
+# echo 'Successfully installed miniconda...'
+# echo -n 'Conda version: '
+# ~/miniconda/bin/conda --version
+# echo -e '\n'
+
+# This block setup user environment.
+# source ~/miniconda/etc/profile.d/conda.sh
+# conda env list
+# echo 'Create fairseq env...'
+# conda create -n fairseq python=3.8
+# conda init $(echo $SHELL | awk -F '/' '{print $NF}')
+# source ~/.bashrc
+# conda env list
+# echo 'Activate fairseq env...'
+# conda activate fairseq
+# conda info --env
+
+# This block install components.
 pip install tensorboardX
 pip install Cython
 pip install soundfile
+sudo apt-get -y install libsndfile1
 # git clone https://github.com/ddlBoJack/fairseq.git
 # cd fairseq
 # git checkout v-ziyangma
+echo "Work directory: $(pwd)"
 export PYTHONPATH=${pwd}:$PYTHONPATH
 echo 'fairseq install...'
 pip install --editable ./
 # python setup.py build_ext --inplace
 echo -e '\n'
 
-source ~/miniconda/etc/profile.d/conda.sh
-echo 'Activate nat env...'
-conda activate fairseq
+# source ~/miniconda/etc/profile.d/conda.sh
+# echo 'Activate fairseq env...'
+# conda activate fairseq
 
 # edit your exp
 prefix_dir=/datablob/users/v-ziyangma
 model_name=data2vec
-exp_name=data2vec_debug1
+exp_name=data2vec_960h_devclean
 
 #edit your config
 config_dir=./config/data2vec/audio/pretraining
@@ -56,6 +64,7 @@ tb_path=${prefix_dir}/log/${model_name}/${exp_name}/tensorboard
 mkdir -p ${tb_path}
 log_file=${prefix_dir}/log/${model_name}/${exp_name}/hydra_train.log
 
+export PYTHONPATH=${pwd}:$PYTHONPATH
 # pretrain
 # python -m debugpy --listen 5678 --wait-for-client fairseq_cli/hydra_train.py  \
 python fairseq_cli/hydra_train.py  \
